b36b29ee4bdeebe28ac853bd390ff46912ee84c7
Jonathan Guerne <jonathan.guerne@gmail.com>
2023-07-09 01:25:56 +0200

    level for matchbox movement test

--- Assets/Scripts/Burnables/Match/MatchMovement.cs
+++ Assets/Scripts/Burnables/Match/MatchMovement.cs
@@ -11,17 +11,25 @@ public class MatchMovement : BurnableMovement
 
     [SerializeField] private float zigAngle = 160f;
 
+    private float currentAngle = 0f;
+
     
-    void FixedUpdate()
+    void Update()
     {
+
         currentTimeInZig += Time.deltaTime;
+
+        // a normalization where 0 is min, timePerZig is max
+        // what is the value of currentAngle if zigAngle is min and -zigAngle is max?
+        
+        float progress = currentTimeInZig / timePerZig;
+        currentAngle = Mathf.Lerp(zigAngle, -zigAngle, progress);
 
         if (currentTimeInZig > timePerZig)
         {
             currentTimeInZig = 0.0f;
             zigAngle = -zigAngle;
         }
-
 
         Vector3 unnormalizedDistance = targetPosition - transform.position;
 
@@ -30,7 +38,7 @@ public class MatchMovement : BurnableMovement
             transform.position,
             // target position is a cominbination of the acutal target position and the zig angle,
             // which is an angle in the xz plane
-            Quaternion.Euler(0, zigAngle, 0) * unnormalizedDistance,
+            Quaternion.Euler(0, currentAngle, 0) * unnormalizedDistance,
             burnable.GetSpeed() * Time.deltaTime
         );
 
