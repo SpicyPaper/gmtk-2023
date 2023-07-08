feffa4eb266d2f9de3f82178af02d4caba7f040a
Farid Abdalla <farid.abdalla.13@gmail.com>
2023-07-09 00:30:49 +0200

    Add GameState and BurnableSpawn Scriptable Object

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField]
-    private GameObject burnablePrefab;
+    public BurnableSpawnConfig spawnConfig;
 
     [SerializeField]
-    private float spawnInterval = 1.0f;
+    private float spawnInterval = 4.0f;
 
     [SerializeField]
     private Vector3 spawnCenter = Vector3.zero;
@@ -18,7 +19,9 @@ public class Spawner : MonoBehaviour
 
     private float spawnTimer = 0.0f;
 
-    void Start() { }
+    void Start()
+    {
+    }
 
     // Update is called once per frame
     void Update() { }
@@ -35,11 +38,31 @@ public class Spawner : MonoBehaviour
 
     private void SpawnBurnable()
     {
-        GameObject burnable = Instantiate(
-            burnablePrefab,
-            GetRandomSpawnPosition(),
-            Quaternion.identity
-        );
+        // Generate a random number between 0 and sum of probabilities in burnablePrefabs
+        LevelSpawnConfig levelConfig = spawnConfig.levelSpawnConfigs.Find(config => config.level == GameController.gameState);
+
+        if (levelConfig != null)
+        {
+            float probSum = levelConfig.burnableSpawnChances.Sum(x => x.spawnProbability);
+            float randomNum = Random.Range(0.0f, probSum);
+            foreach (var item in levelConfig.burnableSpawnChances)
+            {
+                if (randomNum < item.spawnProbability)
+                {
+                    GameObject burnable = Instantiate(
+                        item.burnablePrefab,
+                        GetRandomSpawnPosition(),
+                        Quaternion.identity
+                    );
+                    break;
+                }
+                else
+                {
+                    randomNum -= item.spawnProbability;
+                }
+            }
+        }
+
     }
 
     private Vector3 GetRandomSpawnPosition()
