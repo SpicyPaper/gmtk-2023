202026874fa60440a735fc9076bc584c6d32851e
Farid Abdalla <farid.abdalla.13@gmail.com>
2023-07-09 01:16:05 +0200

    Add spawn interval and number of burnables per lvl

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,7 +11,6 @@ public class GameController : MonoBehaviour
     public static GameState gameState = GameState.Level1;
 
 
-
     // Start is called before the first frame update
     void Start()
     {
